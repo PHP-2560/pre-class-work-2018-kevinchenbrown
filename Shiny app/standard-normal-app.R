94e9d48a9d917c876e2054cde4926c86b0ee3679
Kevin Chen <kevin_chen1@brown.edu>
2018-12-02 19:58:51 -0500

    begin work on creating sample app

--- Shiny app/standard-normal-app.R
+++ Shiny app/standard-normal-app.R
@@ -1,6 +1,18 @@
 library(shiny)
-ui <- fluidPage()
-
-server <- function(input, output) {}
+ui <- fluidPage(
+  titlePanel("Standard Normal Probabilities"),
+  h3("Input a z score below to calculate its correponding p-value."),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("z")),
+    mainPanel(textOutput("p-value"))
+      
+      
+    )
+  )
+  
+server <- function(input, output) {
+  output$p-value <- reactive({pnorm(input$z)})
+}
 
 shinyApp(ui = ui, server = server)
